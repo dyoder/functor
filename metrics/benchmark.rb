4e2f52edec131567909478a537a5c00d1c74c1fb
Matthew King <automatthew@gmail.com>
2009-01-09 06:55:08 -0600

    updated benchmark

--- metrics/benchmark.rb
+++ metrics/benchmark.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 
-$:.unshift "stevedore/lib"
-$:.unshift "#{File.dirname(__FILE__)}/../lib"
+$:.unshift "#{here = File.dirname(__FILE__)}/stevedore/lib"
+$:.unshift "#{here}/../lib"
 require 'stevedore'
 require 'functor'
 
-commit = `git show-ref -s --abbrev HEAD`.chomp
 
-bm = Steve.new "Functor at #{commit}" do
+class FuncFib < Steve
   
+  subject "Fibonacci using Functor"
   before do
     @fib ||= Functor.new do |f|
       f.given( Integer ) { | n | f.call( n - 1 ) + f.call( n - 2 ) }
@@ -16,11 +16,16 @@ bm = Steve.new "Functor at #{commit}" do
       f.given( 1 ) { 1 }
     end
   end
-  
-  measure do
-    [*0..15].map( &@fib )
-  end
-  
 end
 
-Steve.compare_instances( 10, 10)
+fib_13 = FuncFib.new "f(10)" do  
+  measure do
+    100.times { @fib.call(10) }
+  end
+end
+
+fib_15 = FuncFib.new "f(20)" do  
+  measure { @fib.call(20)  }
+end
+
+FuncFib.compare_instances( 4, 8)
