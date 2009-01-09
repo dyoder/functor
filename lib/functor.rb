978b5a1d217439a0ec06a422f696783fddad52ba
Matthew King <automatthew@gmail.com>
2009-01-09 06:05:08 -0600

    instadef now working with standalone functors

--- lib/functor.rb
+++ lib/functor.rb
@@ -3,12 +3,14 @@ require "#{File.dirname(__FILE__)}/object"
 class Functor
   
   module Method
+    
     def self.copy_functors( functors )
       r = {} ; functors.each do | name, functor |
         r[ name ] = functor.clone
       end
       return r
     end
+    
     def self.included( k )
       
       def k.functors
@@ -19,7 +21,7 @@ class Functor
       def k.functor( name, *pattern, &block )
         name = name.to_sym
         f = ( functors[ name ] ||=  Functor.new )
-        f.register( *pattern )
+        f.register( pattern )
         define_method( "functo_#{pattern.hash}", block )
         unless respond_to?( name )
           define_method( name ) do | *args |
@@ -36,7 +38,7 @@ class Functor
       def k.functor_with_self( name, *pattern, &block )
         name = name.to_sym
         f = ( functors[ name ] ||=  Functor.new )
-        f.register( *pattern )
+        f.register( pattern )
         define_method( "functo_#{pattern.hash}", block )
         unless respond_to?( name )
           define_method( name ) do | *args |
@@ -55,24 +57,25 @@ class Functor
   
   
   def initialize( &block )
-    @rules = [] ; yield( self ) if block_given?
+    @patterns = []
+    @associations = []
+    yield( self ) if block_given?
   end
   
   def initialize_copy( from )
-    @rules = from.instance_eval { @rules.clone }
-  end
-  
-  def register( *pattern )
-    @rules << pattern
+    @patterns, @associations = from.instance_eval { [@patterns.clone, @associations.clone] }
   end
   
   def given( *pattern, &action )
     register pattern
-    signature = pattern.hash
-    name = "functo_#{signature}"
+    name = "functo_#{pattern.hash}"
     class << self; self; end.instance_eval do
       define_method( name, action )
     end
+  end
+  
+  def register( pattern )
+    @patterns.unshift pattern
   end
   
   def call( *args, &block )
@@ -88,7 +91,7 @@ class Functor
     
   def match( *args, &block )
     args << block if block_given?
-    pattern = @rules.reverse.find { | p | match?( args, p ) }
+    pattern = @patterns.find { | p | match?( args, p ) }
     raise ArgumentError.new( "No functor matches the given arguments." ) unless pattern
     pattern.hash
   end
@@ -96,11 +99,11 @@ class Functor
   private
   
   def match?( args, pattern )
-    args.zip( pattern ).all? { | arg, rule | pair?( arg, rule ) } if args.length == pattern.length
+    args.zip( pattern ).all? { | arg, pat | pair?( arg, pat ) } if args.length == pattern.length
   end
   
-  def pair?( arg, rule )
-    ( rule.respond_to? :call and rule.call( arg ) ) or rule === arg
+  def pair?( arg, pat )
+    ( pat.respond_to? :call and pat.call( arg ) ) or pat === arg
   end
     
 end
