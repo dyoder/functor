c9c3a660e8a3cb93e6b45c11429ac1405b9052d2
Matthew King <automatthew@gmail.com>
2009-01-09 06:24:08 -0600

    extra with_self test for clarity

--- test/with_self.rb
+++ test/with_self.rb
@@ -6,6 +6,7 @@ class A
   def initialize( x ) ; @bar = x ; end
   functor_with_self( :foo, self, Integer ) { |x| x }
   functor_with_self( :foo, lambda{ |x| x.bar == true }, Integer ) { |s| 'bar' }
+  functor_with_self( :foo, lambda{ |x| x.bar.is_a? String }, Integer ) { |s| 'I be string' }
 end
 
 describe "Functor methods should support allow matching on self" do
@@ -16,6 +17,10 @@ describe "Functor methods should support allow matching on self" do
   
   specify "or by simply providing self as an argument" do
     A.new( false ).foo( 5 ).should == 5
+  end
+  
+  specify "another guard example, for those who need it" do
+    A.new( "me" ).foo( 87 ).should == "I be string"
   end
 
 end
