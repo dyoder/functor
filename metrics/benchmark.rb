956c9f213fbe999d56ea167cd24fec1c7d41ebd0
Matthew King <automatthew@gmail.com>
2009-01-09 07:42:43 -0600

    twiddle benchmark for times

--- metrics/benchmark.rb
+++ metrics/benchmark.rb
@@ -9,6 +9,11 @@ require 'functor'
 class FuncFib < Steve
   
   subject "Fibonacci using Functor"
+  
+  # power 0.8
+  # sig_level 0.05
+  delta 0.01
+  
   before do
     @fib ||= Functor.new do |f|
       f.given( Integer ) { | n | f.call( n - 1 ) + f.call( n - 2 ) }
@@ -18,14 +23,17 @@ class FuncFib < Steve
   end
 end
 
-fib_13 = FuncFib.new "f(10)" do  
+fib_8 = FuncFib.new "f(8)" do  
   measure do
-    100.times { @fib.call(10) }
+    64.times { @fib.call(8) }
   end
 end
 
-fib_15 = FuncFib.new "f(20)" do  
-  measure { @fib.call(20)  }
+fib_16 = FuncFib.new "f(16)" do  
+  measure do
+    2.times { @fib.call(16)  }
+  end
 end
 
-FuncFib.compare_instances( 4, 8)
+# FuncFib.recommend_test_size( 8, 16)
+FuncFib.compare_instances( 8, 128)
