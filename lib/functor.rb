474171940c6eb051dd99a3540a1aa19beeea8d40
Matthew King <automatthew@gmail.com>
2009-01-13 17:31:03 -0600

    working chain defs for functor and with_self

--- lib/functor.rb
+++ lib/functor.rb
@@ -20,40 +20,34 @@ class Functor
           Functor::Method.copy_functors( superclass.functors ) : {}
       end
       
-      def k.functor( name, *pattern, &block )
-        name = name.to_sym
-        f = ( functors[ name ] ||=  Functor.new )
-        f.register( pattern )
-        f.meta_def "functo_#{pattern.hash}" do |caller, *args|
-          caller.instance_exec *args, &block
-        end
-        unless respond_to?( name )
-          define_method( name ) do | *args |
-            begin
-              signature = f.match( *args )
-              f.send "functo_#{signature}", self, *args
-            rescue NoMethodError
-              raise ArgumentError.new( "No functor matches the given arguments for method :#{name}." )
-            end
-          end 
-        end
+      def k.functor( name, *pattern, &action )
+        name = name.to_s
+        old = instance_method(name) if instance_methods.include?( name )           
+        define_method( name, action )
+        newest = instance_method(name)
+        define_method( name ) do | *args |
+          if Functor.match?(args, pattern)
+            newest.bind(self).call(*args)
+          elsif old
+            old.bind(self).call(*args)
+          else
+            raise ArgumentError.new( "No functor matches the given arguments for method :#{name}." )
+          end
+        end 
       end
       
-      def k.functor_with_self( name, *pattern, &block )
-        name = name.to_sym
-        f = ( functors[ name ] ||=  Functor.new )
-        f.register( pattern )
-        f.meta_def "functo_#{pattern.hash}" do |caller, *args|
-          caller.instance_exec *args, &block
-        end
-        unless respond_to?( name )
-          define_method( name ) do | *args |
-            begin
-              signature = f.match( self, *args )
-              f.send "functo_#{signature}", self, *args
-            rescue NoMethodError
-              raise ArgumentError.new( "No functor matches the given arguments for method :#{name}." )
-            end
+      def k.functor_with_self( name, *pattern, &action )
+        name = name.to_s
+        old = instance_method(name) if instance_methods.include?( name )           
+        define_method( name, action )
+        newest = instance_method(name)
+        define_method( name ) do | *args |
+          if Functor.match?([self] + args, pattern)
+            newest.bind(self).call(*args)
+          elsif old
+            old.bind(self).call(*args)
+          else
+            raise ArgumentError.new( "No functor matches the given arguments for method :#{name}." )
           end
         end
       end
@@ -64,7 +58,6 @@ class Functor
   
   def initialize( &block )
     @patterns = []
-    @associations = {}
     yield( self ) if block_given?
   end
   
@@ -96,18 +89,19 @@ class Functor
   def to_proc ; lambda { |*args| self.call( *args ) } ; end
     
   def match( *args, &block )
-    arg_sig = args.hash
-    if pattern_sig = @associations[arg_sig]
-      pattern_sig
-    else
-      args << block if block_given?
-      pattern = @patterns.find { | p | match?( args, p ) }
-      raise ArgumentError.new( "No functor matches the given arguments." ) unless pattern
-      @associations[arg_sig] = pattern.hash
-    end
+    args << block if block_given?
+    pattern = @patterns.find { | p | match?( args, p ) }
+    raise ArgumentError.new( "No functor matches the given arguments." ) unless pattern
   end
   
-  private
+  
+  def self.match?( args, pattern )
+    args.zip( pattern ).all? { | arg, pat | pair?( arg, pat ) } if args.length == pattern.length
+  end
+  
+  def self.pair?( arg, pat )
+    ( pat.respond_to? :call and pat.call( arg ) ) or pat === arg
+  end
   
   def match?( args, pattern )
     args.zip( pattern ).all? { | arg, pat | pair?( arg, pat ) } if args.length == pattern.length
