7957663d9da069d1e733fce357a146bc18a8479a
Matthew King <automatthew@gmail.com>
2009-01-17 16:01:59 -0600

    tiered caches based on exponential increasing count

--- lib/functor.rb
+++ lib/functor.rb
@@ -20,20 +20,52 @@ class Functor
         @functor_cache_size = val if val
       end
       
+      def k.functor_cache_base(val=nil)
+        if val
+          raise ArgumentError, "I need an Integer" unless val.is_a? Integer
+          @functor_cache_base = val
+        else
+          @functor_cache_base ||= 10
+        end
+      end
+      
+      def k.functor_cache_1
+        @functor_cache_1 ||= {}
+      end
+      
+      def k.functor_cache_2
+        @functor_cache_2 ||= {}
+      end
+      
+      def k.functor_cache_3
+        @functor_cache_3 ||= {}
+      end
+      
       def k.functor( name, *pattern, &action )
         name = name.to_s
-        cache = (functor_cache[name] ||= {})
+        cache_1, cache_2, cache_3 = (functor_cache_1[name] ||= {}), (functor_cache_2[name] ||= {}), (functor_cache_3[name] ||= {})
         cache_size = functor_cache_size
         old = instance_method(name) if instance_methods.include?( name )           
         define_method( name, action )
         newest = instance_method(name)
         define_method( name ) do | *args |
           signature = args.hash
-          if meth = cache[signature]
+          if meth = cache_3[signature]
             meth.bind(self).call(*args)
+          elsif meth = cache_2[signature]
+            cache_3 = {} if cache_size && cache_3.size > cache_size
+            count = meth.last
+            cache_3[signature] if count > functor_cache_base ** 2
+            count += 1
+          elsif meth = cache_1[signature]
+            cache_2 = {} if cache_size && cache_2.size > cache_size
+            count = meth.last
+            cache_2[signature] = meth if count > functor_cache_base 
+            count += 1
+            meth.first.bind(self).call(*args)
           elsif Functor.match?(args, pattern)
-            cache = {} if cache_size && cache.size > cache_size
-            cache[signature] = newest
+            cache_1 = {} if cache_size && cache_1.size > cache_size
+            cache_1[signature] = [newest, 0]
             newest.bind(self).call(*args)
           elsif old
             old.bind(self).call(*args)
